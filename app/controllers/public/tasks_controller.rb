da42c347abed2f088bf411b41d0c518a12adcb7f
step1121 <au5.iphonex3@outlook.jp>
2022-08-09 10:45:10 +0900

    [public]tasks-controller

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -15,9 +15,9 @@ class Public::TasksController < ApplicationController
       redirect_to vision_path(@vision)
     else
       @tasks = @vision.tasks.order(completion_on: "ASC")
-      # 未完了TASK
+      # 未完了のTASK
       @tasks_yet = @tasks.yet
-      # 完了TASK
+      # 完了のTASK
       @tasks_complete =@tasks.complete
       render 'public/visions/show'
     end
@@ -36,7 +36,7 @@ class Public::TasksController < ApplicationController
     @tasks = @vision.tasks.all
     # TASKステータスの切り替え
     @task.completion_status == false ? (@task.update(completion_status: true)) : (@task.update(completion_status: false))
-    # 全TASK完了ステータスとVISION達成ステータスの連動
+    # 全てTask完了したとき,VISION達成ステータスが変わる動作
     @tasks.where(completion_status: true).count != @tasks.count ? (@vision.update(finish_status: false)) : (@vision.update(finish_status: true))
     redirect_to request.referer
   end
